2f65c3e7b396162cd96805699b98db4a7eb0dabc
monilpatel12 <122998965+monilpatel12@users.noreply.github.com>
2023-02-08 10:51:08 -0500

    Added sub function After declaring sub function in void sub is added to case 2 in switch statement. A whole new sub fuction was added to take user input and perform subtraction for 3 numbers.

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -3,6 +3,7 @@
 void printWelcomeMenu();
 void printOptions();
 void add();
+void sub();
 
 
 void main() {
@@ -20,6 +21,8 @@ void main() {
 	{
 	case 1:
 		add();
+	case 2:
+		sub();
 	}
 
 }
@@ -45,5 +48,17 @@ void add() {
 	printf("Enter the third value:");
 	scanf_s("%lf", &num3);
 	result = num1 + num2+num3;
-	printf("%lf + %lf = %lf\n", num1, num2, result);
+	printf("%lf + %lf + %lf = %lf\n", num1, num2, num3, result);
+}
+
+void sub() {
+	double num1, num2, num3, result;
+	printf("Enter the first value:");
+	scanf_s("%lf", &num1);
+	printf("Enter the second value:");
+	scanf_s("%lf", &num2);
+	printf("Enter the third value:");
+	scanf_s("%lf", &num3);
+	result = num1 - num2 - num3;
+	printf("%lf - %lf - %lf = %lf\n", num1, num2, num3, result);
 }
